03e888b79d3b0aa73118b9459adc4ec2cb34fbe4
Mokhtar Lahjaily <mlahjaily25@gmail.com>
2025-10-24 18:46:00 +0100

    client service

---
+++ customer-service/src/main/java/ma/lahjaily/customerservice/config/RestRepositoryConfig.java
@@ -0,0 +1,15 @@
+package ma.lahjaily.customerservice.config;
+
+import ma.lahjaily.customerservice.entities.Customer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class RestRepositoryConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(Customer.class);
+    }
+}
